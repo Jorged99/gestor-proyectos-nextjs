f06c1fb498f3bf234c06cfe3298178ef815155e0
Jorged99 <jorgeald724@gmail.com>
2026-03-25 22:52:22 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
 import '../styles/globals.css';
 import { AuthProvider } from '../context/AuthContext';
+import Navbar from '../components/Navbar';
 
 export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <Component {...pageProps} />
+      <div className="flex min-h-screen bg-gray-50">
+        {/* El Navbar ahora es una barra lateral izquierda */}
+        <Navbar />
+        
+        {/* Usamos ml-64 (margin-left) para empujar el contenido hacia la derecha, dejando espacio para el menú */}
+        <main className="flex-1 ml-64 w-full">
+          <Component {...pageProps} />
+        </main>
+      </div>
     </AuthProvider>
   );
 }
